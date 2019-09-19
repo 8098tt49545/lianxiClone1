5780975b0173ca8c53eeae039871d7b2ab2ad898
luqiang <809849545@qq.com>
2019-09-19 12:03:41 +0800

    qee

---
+++ Dashunche2Tests.m
@@ -0,0 +1,54 @@
+//
+//  Dashunche2Tests.m
+//  Dashunche2Tests
+//
+//  Created by 杨少军 on 2016/11/5.
+//  Copyright © 2016年 杨少军. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dashunche2Tests : XCTestCase
+
+@end
+
+@implementation Dashunche2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+- (void)textLearnGit
+{
+    // 学习git
+    int y = 1;
+    y = 2;
+    if (y) {
+        [self testExample];
+    }else {
+        // 测试添加第二版本
+        //链接远程仓库github 成功
+        //再次链接储存库
+        // git push origin 
+
+    }
+    
+}
+@end
