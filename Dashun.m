e73e461864482d134c753e00845b5d3cb4770177
luqiang <809849545@qq.com>
2019-09-20 10:27:19 +0800

    text

---
+++ Dashun.m
@@ -0,0 +1,60 @@
+//
+//  Dashunche2Tests.m
+//  Dashunche2Tests
+//
+//  Created by 杨少军 on 2016/11/5.
+//  Copyright © 2016年 杨少军. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dashunche2Tests : XCTestCase
+
+@end
+
+@implementation Dashunche2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+- (void)textLearnGit
+{
+    // 学习git
+    int y = 1;
+    y = 2;
+    if (y) {
+        [self testExample];
+    }else {
+        
+        // start working devvv
+        // begain work bug issue-101
+        // xiugaile xianzai de wenjian bangzhu shixianxiugai bug
+        // begain bug stash
+        // start working devvv
+        // begain work bug issue-101
+    }
+    
+}
+- (void)bugWenjian
+{
+    // issue001 xiugai
+}
+@end
