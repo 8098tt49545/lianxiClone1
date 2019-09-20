fd9d5cd4228754b66869f2ae285d7a4ed5de14df
luqiang <809849545@qq.com>
2019-09-20 16:15:57 +0800

    kong

--- Dashunche2Tests.m
+++ Dashunche2Tests.m
@@ -16,11 +16,7 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    // dev分支 提交版本，推送提交
-    
-    // 成功建立dev 并提交推送
-    // 我自己修改的测试1
+    // kong
 }
 
 
